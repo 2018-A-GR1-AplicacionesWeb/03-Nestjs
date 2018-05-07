ee6b9960b87977631248172a9a5df6cd2a2e6ab9
Adrian Eguez <eadepto@hotmail.com>
2018-05-07 09:57:32 -0500

    index html y controlador

--- Ejemplo/src/app.controller.ts
+++ Ejemplo/src/app.controller.ts
@@ -1,9 +1,29 @@
-import { Get, Controller } from '@nestjs/common';
+import {Get, Controller} from '@nestjs/common';
+
+const fs = require("fs");
 
 @Controller()
 export class AppController {
-  @Get()
-  root(): string {
-    return 'Hola mundo';
-  }
+    @Get()
+    root(): string {
+        console.log('1 Inicio');
+        let contenidoHtml = '';
+        console.log('2 contenidoHtml',contenidoHtml);
+        fs.readFile(
+            __dirname + '/html/Index.html',
+            'utf8',
+            (error, contenidoDelArchivo) => {
+                console.log('3 Respondio');
+                if (error) {
+                    console.log('4 Error', error);
+                } else {
+                    contenidoHtml = contenidoDelArchivo;
+                    console.log('4 contenidoHtml',contenidoHtml);
+                }
+            }
+        );
+        console.log('5 contenidoHtml',contenidoHtml);
+        console.log('6 Termino');
+        return contenidoHtml;
+    }
 }
--- Ejemplo/src/main.ts
+++ Ejemplo/src/main.ts
@@ -2,7 +2,7 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 
 const cookieParser = require('cookie-parser');
-
+// npm i cookie-parser --save
 async function bootstrap() {
 
     const app = await NestFactory.create(AppModule);
