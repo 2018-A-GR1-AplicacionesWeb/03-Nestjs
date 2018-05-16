daeb5cd5af1743c4cc84040581ac7b8b6e546b22
Adrian Eguez <eadepto@hotmail.com>
2018-05-16 12:01:42 -0500

    nest 5.0.0

--- Ejemplo/src/app.module.ts
+++ Ejemplo/src/app.module.ts
@@ -3,12 +3,15 @@ import {AppController} from './app.controller';
 import {UsuarioController} from "./usuario.controller";
 import {ParametrosController} from "./parametros.controller";
 import {LogMiddleware} from "./log.middleware";
-
+import {UsuarioService} from "./usuario.service";
 
 
 @Module({
     imports: [ // OtrosModulos
 
+    ],
+    providers: [
+        UsuarioService
     ],
     controllers: [ // Controladores
         AppController,
