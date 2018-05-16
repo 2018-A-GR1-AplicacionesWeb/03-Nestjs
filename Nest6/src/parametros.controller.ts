77d2153d0329107d7fdbe469100a233ddc61d99b
Adrian Eguez <eadepto@hotmail.com>
2018-05-16 12:19:43 -0500

    parametros controller

--- Nest6/src/parametros.controller.ts
+++ Nest6/src/parametros.controller.ts
@@ -1,7 +1,12 @@
 import {Body, Controller, Get, Headers, Param, Post, Query, Req, Res} from "@nestjs/common";
+import {UsuarioService} from "./usuario.service";
 
 @Controller('Parametros')
 export class ParametrosController {
+
+    constructor(private _usuarioService: UsuarioService) {
+
+    }
 
     @Post('devolver/:id/:modelo')
     devolverParametros(
@@ -38,6 +43,24 @@ export class ParametrosController {
         };
         console.log(respuesta);
         return response.redirect('/Usuario/mostrar');///Usuario/mostrar
+    }
+
+    @Post('crearUsuario')
+    crearUsuario(
+        @Req() request,
+        @Res() response
+    ) {
+        const nuevoUsuario = {
+            nombre: request.query.nombre,
+            apellido: request.query.apellido,
+            edad: request.query.edad
+        };
+
+        const usuarioCreado = this._usuarioService.crearUsuario(nuevoUsuario);
+
+        return response
+            .status(201)
+            .send(usuarioCreado);
     }
 
 }
