bfc5fdad093327595290b7ca25523cfdc0bd9945
Adrian Eguez <eadepto@hotmail.com>
2018-06-11 10:19:21 -0500

    autorizacion

---
+++ Nest6/src/auth/auth.controller.ts
@@ -0,0 +1,49 @@
+import {BadRequestException, Body, Controller, Post} from "@nestjs/common";
+import {JwtService} from "../servicios/jwt.service";
+
+
+@Controller('Auth')
+export class AuthController {
+    constructor(
+        private _jwtService: JwtService
+    ) {
+    }
+
+    @Post('login')
+    login(
+        @Body('username') username: string,
+        @Body('password') password: string,
+    ) {
+        const enviaUsername = username;
+        const enviaPassword = password;
+        const enviarParametros = enviaPassword && enviaUsername;
+
+        if (enviarParametros) {
+            if (username === 'adrianeguez' &&
+                password === '1234') {
+
+                const payload = {
+                    username: username
+                };
+
+                const respuestaToken = {
+                    jwt: this._jwtService.emitirToken(payload)
+                };
+
+                return respuestaToken;
+
+            } else {
+                throw new BadRequestException({
+                    mensaje: 'Credenciales invalidas'
+                })
+            }
+
+        } else {
+            throw new BadRequestException({
+                mensaje: 'No envia parametros'
+            })
+        }
+
+    }
+
+}
